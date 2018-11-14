adbfcb9cee0ed0510a42c77a3b8dfc7c782c9d5c
Vitaliy Kuzmin <inknex@gmail.com>
2018-11-14 20:28:25 +0200

    Create new module

---
+++ app/code/Kram/Demo/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Kram_Demo',
+        __DIR__
+    );
